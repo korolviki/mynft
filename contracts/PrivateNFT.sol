8aba4caa4bce8e57e185400d39760ba267551b48
korolviki korolviki <viktoriakorolova642@gmail.com>
2024-08-07 13:26:49 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("koro","krr")
+        ERC721("koro","kor")
         Ownable(initialOwner)
     {}
 
